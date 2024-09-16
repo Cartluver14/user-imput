0506c3a9bb4628552df835f13ddda2f4593b3f06
North Harrington <339713067@GoTVDSB.ca>
2024-09-16 09:40:17 -0400

    Add project files.

---
+++ user imput/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace user_imput
+{
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            //North
+            string name;
+            int age;
+
+            Console.WriteLine("Hello. What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("press Enter to continue");
+            Console.ReadLine();
+            Console.WriteLine("Hi " + name + " How old are you?");
+            age = Convert.ToInt32(Console.ReadLine());
+           
+        }
+    }
+}
