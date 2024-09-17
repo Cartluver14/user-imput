3896de2a9404d6a0057cfc8a3c0553fc03cbdd44
North Harrington <339713067@GoTVDSB.ca>
2024-09-17 09:40:05 -0400

    almost done

--- user imput/Program.cs
+++ user imput/Program.cs
@@ -12,16 +12,28 @@ namespace user_imput
         static void Main(string[] args)
         {
             //North
-            string name;
-            int age;
+            string name, pizzatoppping;
+            int age = 11;
+            double price1, price2, tax, subtotal, total;
+
+
+
+            name = "45";
+            Console.WriteLine(age);
 
             Console.WriteLine("Hello. What is your name?");
             name = Console.ReadLine();
             Console.WriteLine("press Enter to continue");
             Console.ReadLine();
             Console.WriteLine("Hi " + name + " How old are you?");
-            age = Convert.ToInt32(Console.ReadLine());
-           
+            Int32.TryParse(Console.ReadLine(), out age);
+            Console.WriteLine(age);
+            Console.ReadLine();
+
+            Console.WriteLine(" Hey again, what is your favorate pizza topping? ");
+            pizzatoppping = Console.ReadLine();
+            Console.WriteLine("Funny my favorate pizza toping is also " + pizzatoppping);
+            Console.ReadLine();
         }
     }
 }
